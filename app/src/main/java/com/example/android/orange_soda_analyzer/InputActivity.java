2b58f47a13735bcd1ff5445112e5d539b3c28106
gundralaa <mathgame99@gmail.com>
2018-07-21 16:04:54 -0700

    Initial App Repo

---
+++ app/src/main/java/com/example/android/orange_soda_analyzer/InputActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.orange_soda_analyzer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+}
